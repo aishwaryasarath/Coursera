aaf0471b6003f00674e459c9296625c87fe5a2b8
Aiswarya Sarath <aishwaryasarath@gmail.com>
2020-03-29 18:13:56 -0500

    added a free memory filesc

--- course6/project4/report_email.py
+++ course6/project4/report_email.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 import reports
 import os
-import datatime
+import datetime
 
 file_names = []
-file_names = os.listdir(os.chdir(os.getcwd()+"/supp$
+file_names = os.listdir(os.chdir(os.getcwd()+"/supplier-data/descriptions"))
 summary = ""
-title = "Processed update on" datetime. datetime.$
+title = "Processed Update on "
+date = datetime.datetime.now().strftime("%B %d, %Y")
 for file in file_names:
   with open(file) as fp:
     for index, line in enumerate(fp):
       if index == 0:
-        summary+= line.strip()
+        summary+= "name: " +line.strip()
       elif index == 1:
-        summary += line.strip()
+        summary += "weight: "+ line.strip()
       summary+= "<br/>"
-reports.generate_report("/tmp/processed.pdf",title,$
-
+      #summary += "/n"
+print(title+date)
+print(summary)
+print(os.getcwd())
+reports.generate_report("../../tmp/processed.pdf",title,summary)
 
